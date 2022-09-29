24f3ad511137ee39dc7eb7b1d2a3b51368154ff0
Vita Koval <mayevit@hotmail.com>
2022-09-29 15:04:53 +0300

    fixed

--- src/components/FeedbackForm/FeedbackFormHook.jsx
+++ src/components/FeedbackForm/FeedbackFormHook.jsx
@@ -4,7 +4,7 @@ import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
 import { Statistics } from '../Statistics/Statistics';
 import { Notification } from '../Notification/Notification';
 
- function FeedbackForm() {
+function FeedbackForm() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -23,6 +23,7 @@ import { Notification } from '../Notification/Notification';
       case 'bad':
         setBad(bad => bad + 1);
         break;
+      default: ;
     }
   }
 
@@ -59,6 +60,6 @@ import { Notification } from '../Notification/Notification';
       </Section>
     </div>
   );
- }
+}
 
 export default FeedbackForm;
